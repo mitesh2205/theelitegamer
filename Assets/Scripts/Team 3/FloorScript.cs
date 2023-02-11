e798632c85664716513548f6755bfb6bbbbb0b5f
Saket Ozarkar <ozarkar@usc.edu>
2023-02-11 15:22:26 -0800

    Added Jigsaw Scripts and Prefabs in team 3 folder

---
+++ Assets/Scripts/Team 3/FloorScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorScript : MonoBehaviour
+{
+     private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("bullet")){
+           Destroy(collision.gameObject, 2f);
+        }
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
