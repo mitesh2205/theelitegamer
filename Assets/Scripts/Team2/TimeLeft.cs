921085d2ceaedd34e36c59e6b7f40b0b404470b8
mitesh2205 <48359593+mitesh2205@users.noreply.github.com>
2023-02-15 22:05:21 -0800

    Final Merge

---
+++ Assets/Scripts/Team2/TimeLeft.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeLeft : MonoBehaviour
+{
+    public static float ScoreValue = 1.5f;
+    Text score;
+    Text jetpack;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = GetComponent<Text>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = "Fuel left in jetpack (J Key): " + ScoreValue;
+
+        if(ScoreValue <= 0)
+        {
+            score.text = "Fuel left in jetpack (J Key): 0";
+        }
+    }
+}
+
+
