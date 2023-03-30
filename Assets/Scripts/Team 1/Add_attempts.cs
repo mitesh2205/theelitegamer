6dab98f4e7aec30147332704ad868058b7b70a3b
shrey patel <38159468+shrey82@users.noreply.github.com>
2023-03-30 15:25:02 -0700

    update attempts counter
    max attempts update

--- Assets/Scripts/Team 1/Add_attempts.cs
+++ Assets/Scripts/Team 1/Add_attempts.cs
@@ -9,8 +9,10 @@ public class Add_attempts : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            Attempts_Counter.attempts += 1;
-            Destroy(gameObject);
+            if(Attempts_Counter.attempts < 5){
+                Attempts_Counter.attempts += 1;
+                Destroy(gameObject);
+            }
             // SpriteRenderer renderer = gameObject.GetComponent<SpriteRenderer>();
             // renderer.enabled = false;
         }
